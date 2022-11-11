182166c51c4d28b5892fd2b14a53a546b2af96b7
ChristopheClaustre <christophe.claustre.31@gmail.com>
2022-11-11 20:46:44 +0100

    [FCT] ServerWidget : Apply ServerSettings when set

--- scripts/ServerSettings.gd
+++ scripts/ServerSettings.gd
@@ -1,12 +1,15 @@
 extends Resource
+class_name ServerSettings
 
 
-# connection settings
-var host = ""
-var port = -1
-var use_ssl = false
-var verify_host = true
+# server settings
+var host := ""
+var port := 8080
+var use_ssl := true
+var verify_host := true
+var username := ""
+var password := ""
 # filter settings
 var type_list : BlackOrWhiteList = BlackOrWhiteList.new()
 var name_list : BlackOrWhiteList = BlackOrWhiteList.new()
-var plan = -1
+var plan := -1
--- scripts/ServerWidget.gd
+++ scripts/ServerWidget.gd
@@ -1,9 +1,32 @@
 extends Control
 
 
+var _serverSettings := ServerSettings.new() setget _set_serverSettings
+
+
+onready var _domoticzMainNode := $"%DomoticzMainNode"
+onready var _updateButton := $"%UpdateButton"
+onready var _autoUpdateCheckBox := $"%AutoUpdateCheckBox"
+
+
 func _on_UpdateButton_pressed():
 	$DomoticzMainNode.request_devices_list()
 
 
 func _on_DomoticzMainNode_devices_list_retrieved(devices):
 	$DevicesList.setList(devices)
+
+
+func _set_serverSettings(value):
+	_serverSettings = value
+	updateUI()
+
+
+func updateUI():
+	_autoUpdateCheckBox.pressed = _serverSettings.auto_update_on_tab
+	_domoticzMainNode.host = _serverSettings.host
+	_domoticzMainNode.port = _serverSettings.port
+	_domoticzMainNode.use_ssl = _serverSettings.use_ssl
+	_domoticzMainNode.verify_host = _serverSettings.verify_host
+	_domoticzMainNode.username_encoded = Marshalls.utf8_to_base64(_serverSettings.username)
+	_domoticzMainNode.password_encoded = Marshalls.utf8_to_base64(_serverSettings.password)
